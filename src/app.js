795a4b1027a5f37eef15a4c6cf66f9c515aa34f4
Colin Williams <colin.williams.dev@gmail.com>
2024-02-11 20:02:19 -0600

    feat: consume app env vars

--- src/app.js
+++ src/app.js
@@ -1,3 +1,12 @@
 import dotenv from "dotenv";
 import { App, createNodeMiddleware  } from "octokit";
 import http from "http";
+
+dotenv.config();
+
+const appId = process.env.APP_ID;
+const webhookSecret = process.env.WEBHOOK_SECRET;
+const privateKey = Buffer
+  .from(process.env.PRIVATE_KEY, "base64")
+  .toString("ascii");
+
