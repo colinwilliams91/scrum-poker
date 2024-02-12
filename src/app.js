9e7082bc2abb34b054fd8ffa47b4778a7e97d0fa
Colin Williams <colin.williams.dev@gmail.com>
2024-02-11 20:42:29 -0600

    chore: TODO GET backlog repo and AUTH pseudo code

--- src/app.js
+++ src/app.js
@@ -10,3 +10,18 @@ const privateKey = Buffer
   .from(process.env.PRIVATE_KEY, "base64")
   .toString("ascii");
 
+/* Octokit App Class */
+const app = new App({
+  appId: appId,
+  privateKey: privateKey,
+  webhooks: {
+    secret: webhookSecret
+  },
+});
+
+// TODO: Add a repo to the application watched repositories to GET backlog
+
+// app.oauth.on("token", async ({ token, octokit }) => {
+//   const { data } = await octokit.request("GET /user");
+//   console.log(`Token retrieved for ${data.login}`);
+// });
