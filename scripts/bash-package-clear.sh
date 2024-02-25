7d2bd46af4947525aca08c88cf565ca1a3bf1bb7
Colin Williams <colin.williams.dev@gmail.com>
2024-02-25 14:53:54 -0600

    refactor: handle conditional for exec permissions .sh & .ps1

--- scripts/bash-package-clear.sh
+++ scripts/bash-package-clear.sh
@@ -1,7 +1,15 @@
+#!/bin/bash
+
 # This will clear the node_modules cached packages and reinstall for Unix/Bash/ZSH
 # Run from root
 
-chmod +x ./scripts/zsh-clear-build.sh
+SCRIPT="./scripts/bash-package-clear.sh"
+
+if ! [[ -x "$SCRIPT" ]]; then
+    echo "First time running script..."
+    echo "Setting executable permission..."
+    chmod +x "$SCRIPT"
+fi
 
 rm -rf ./node_modules
 
--- scripts/clean-build.ps1
+++ scripts/clean-build.ps1
@@ -1,6 +1,15 @@
 # This will clear distributed/compiled javascript files (from typescript) and re-compile (to re-sync and garbage collect stale files)
 # Run from root
 
+# Check if script execution is blocked due to execution policy
+if ((Get-ExecutionPolicy) -eq "Restricted") {
+  Write-Host "Script execution is currently blocked due to the execution policy."
+  Write-Host "We will now bypass the execution policy for this script."
+
+  # Bypass the execution policy for this session and execute the script
+  & powershell.exe -ExecutionPolicy Bypass -File .\YourScript.ps1
+}
+
 Remove-Item -r -fo dist/
 
 npm run build
